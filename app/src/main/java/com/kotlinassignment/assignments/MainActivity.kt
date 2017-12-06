e2035c3a774a89bc9b2f1c82615301cee6485a9c
harshita ahuja <harshita.ahuja@metacube.com>
2017-12-06 18:44:07 +0530

    - Fragment in activity - List fragment

--- app/src/main/java/com/kotlinassignment/assignments/MainActivity.kt
+++ app/src/main/java/com/kotlinassignment/assignments/MainActivity.kt
@@ -2,13 +2,21 @@ package com.kotlinassignment.assignments
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.kotlinassignment.assignments.dummy.DummyContent
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , ItemFragment.OnListFragmentInteractionListener {
+    override fun onListFragmentInteraction(item: DummyContent.DummyItem) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +27,20 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        showListButton.setOnClickListener{ view ->
+            supportFragmentManager.inTransaction {
+                add(R.id.frameLayoutContent, ItemFragment.Companion.newInstance(1))
+            }
+        }
+
+
+    }
+
+    inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> Unit) {
+        val fragmentTransaction = beginTransaction()
+        fragmentTransaction.func()
+        fragmentTransaction.commit()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
